5b8a1f30105bc40a3d431d0841ba591a34ef9df9
Charles Pawling <cpawling@umd.edu>
2021-03-26 11:43:46 -0400

    i hate programing (js update)

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 const staticFolder = 'public';
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
