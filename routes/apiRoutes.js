9255eab423aef90bd2eb63195b9bbc2b8c59b65d
Charles Pawling <cpawling@umd.edu>
2021-04-08 13:41:25 -0400

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -228,26 +228,4 @@ router.get('/custom', async (req, res) => {
   }
 });
 
-
-/// NEW TABLE API ENDPOINT ///
-router.get("/macroChart", async (req, res) => {
-  try {
-      const meals = await db.Meals.findAll();
-      const macros = await db.Macros.findAll();
-      const mealTable = meals.map(meal => {
-        const macrosForMeal = macros.find(macro => macro.meal_id === meal.meal_id)
-        return {
-          id: meal.meal_id,
-          name: meal.meal_name,
-          sodium: macrosForMeal.sodium
-          .... etc
-        }
-      })
-      res.json(mealTable);
-  } catch (err) {
-    console.error(err);
-    res.error("Server error");
-  }
-});
-
 export default router;
