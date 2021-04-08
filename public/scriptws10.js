5975a031cdcbd6cde14cb255ebcdc832fd6978d8
Charles Pawling <cpawling@umd.edu>
2021-04-08 13:32:28 -0400

    Update scriptws10.js

--- public/scriptws10.js
+++ public/scriptws10.js
@@ -16,8 +16,7 @@ async function dining() {
   const targettable = document.querySelector('.table');
 
   arraydata.forEach((element) => {
-    console.log(element.dining);
-    console.log(element.hall_address);
+    console.log(element);
     const appendelement = document.createElement('tr');
     const {mealID} = element.macro_id;
     const {calories} = element.calories;
